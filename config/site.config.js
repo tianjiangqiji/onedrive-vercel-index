84e0b370aacb6e106417bdc5cf5a176c08f61c2c
tianjiangqiji <36837690+tianjiangqiji@users.noreply.github.com>
2022-07-12 11:24:19 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/protect', '/文档/图图'],
+  protectedRoutes: ['/protect', '/图图'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:tianjiangqiji@163.com',

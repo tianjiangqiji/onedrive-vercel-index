17069a311f4ec453740262ab72b01f91f51c4d46
tianjiangqiji <36837690+tianjiangqiji@users.noreply.github.com>
2023-07-08 12:34:46 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -7,63 +7,63 @@ module.exports = {
   // Make sure this is exactly the same as the email address you use to sign into your Microsoft account.
   // You can also put this in your Vercel's environment variable 'NEXT_PUBLIC_USER_PRINCIPLE_NAME' if you worry about
   // your email being exposed in public.
-  userPrincipalName: process.env.NEXT_PUBLIC_USER_PRINCIPLE_NAME || 'tjqj@tianjiangqiji.onmicrosoft.com',
+  userPrincipalName: process.env。NEXT_PUBLIC_USER_PRINCIPLE_NAME || 'skyjee@skyjee.onmicrosoft.com'，
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
-  icon: '/icons/280.png',
+  icon: '/icons/280.png'，
 
   // The name of your website. Present alongside your icon.
-  title: "Skyjee's Cloud",
+  title: "Skyjee's Cloud"，
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
-  baseDirectory: '/共享云',
+  baseDirectory: '/'，
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
-  maxItems: 100,
+  maxItems: 100，
 
   // [OPTIONAL] We use Google Fonts natively for font customisations.
   // You can check and generate the required links and names at https://fonts.google.com.
   // googleFontSans - the sans serif font used in onedrive-vercel-index.
-  googleFontSans: 'Inter',
+  googleFontSans: 'Inter'，
   // googleFontMono - the monospace font used in onedrive-vercel-index.
-  googleFontMono: 'Fira Mono',
+  googleFontMono: 'Fira Mono'，
   // googleFontLinks -  an array of links for referencing the google font assets.
-  googleFontLinks: ['https://fonts.googleapis.com/css2?family=Fira+Mono&family=Inter:wght@400;500;700&display=swap'],
+  googleFontLinks: ['https://fonts.googleapis.com/css2?family=Fira+Mono&family=Inter:wght@400;500;700&display=swap']，
 
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://github.com/tianjiangqiji/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a>. Made with ❤ by Skyjee.',
-    '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="43" height="20" role="img" aria-label="你好啊"><title>你好啊</title><linearGradient id="s" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><clipPath id="r"><rect width="43" height="20" rx="3" fill="#fff"/></clipPath><g clip-path="url(#r)"><rect width="0" height="20" fill="#fe7d37"/><rect x="0" width="43" height="20" fill="#fe7d37"/><rect width="43" height="20" fill="url(#s)"/></g><g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110"><text aria-hidden="true" x="215" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="330">你好啊</text><text x="215" y="140" transform="scale(.1)" fill="#fff" textLength="330">你好啊</text></g></svg>',
+    'Powered by <a href="https://github.com/tianjiangqiji/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a>. Made with ❤ by Skyjee.'，
+    '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="43" height="20" role="img" aria-label="你好啊"><title>你好啊</title><linearGradient id="s" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><clipPath id="r"><rect width="43" height="20" rx="3" fill="#fff"/></clipPath><g clip-path="url(#r)"><rect width="0" height="20" fill="#fe7d37"/><rect x="0" width="43" height="20" fill="#fe7d37"/><rect width="43" height="20" fill="url(#s)"/></g><g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110"><text aria-hidden="true" x="215" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="330">你好啊</text><text x="215" y="140" transform="scale(.1)" fill="#fff" textLength="330">你好啊</text></g></svg>'，
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/protect', '/图图'],
+  protectedRoutes: ['/protect'， '/图图']，
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'mailto:tianjiangqiji@163.com',
+  email: 'mailto:tianjiangqiji@163.com'，
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'GitHub',
-      link: 'https://github.com/tianjiangqiji',
-    },
+      name: 'GitHub'，
+      link: 'https://github.com/tianjiangqiji'，
+    }，
     {
-      name: 'QQ',
-      link: 'http://wpa.qq.com/msgrd?v=3&uin=2590638513&site=qq&menu=yes',
-    },
+      name: 'QQ'，
+      link: 'http://wpa.qq.com/msgrd?v=3&uin=2590638513&site=qq&menu=yes'，
+    }，
     {
-      name: 'cloud',
-      link: 'http://pan.tianjiangqiji.top/',
-    },
-  ],
+      name: 'cloud'，
+      link: 'http://pan.skyjee.cn/'，
+    }，
+  ]，
 
-  // This is a day.js-style datetime format string to format datetimes in the app. Ref to
+  // This is 1 天之前.js-style datetime format string to format datetimes in the app. Ref to
   // https://day.js.org/docs/en/display/format for detailed specification. The default value is ISO 8601 full datetime
   // without timezone and replacing T with space.
-  datetimeFormat: 'YYYY-MM-DD HH:mm:ss',
+  datetimeFormat: 'YYYY-MM-DD HH:mm:ss'，
 }
